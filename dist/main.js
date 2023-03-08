c6ea937fdd9cf39cd312e3ef037ba866d0e17bf5
Kellen Green <me@kellengreen.com>
2023-03-08 12:23:40 -0800

    1678307020

--- dist/main.js
+++
@@ -1,4 +0,0 @@
-import addTime from "./addTime";
-const out = addTime({ a: 0 });
-console.log(out);
-console.log(2);
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ import addTime from "./addTime";
 
 const out = addTime({ a: 0 });
 console.log(out);
-console.log(2);
+console.log(3);
